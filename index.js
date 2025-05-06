6a39922060891363c3f0a4114b93ed70af84d69f
Дмитрий Чехонадских <112265709+loseex@users.noreply.github.com>
2025-05-06 16:05:25 +0500

    upd

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
+class Modal {
+  static #key = "aria-hidden";
+
+  static open(node) {
+    node.setAttribute(this.#key, "false");
+    this.#subscribe_to_close(node);
+  }
+
+  static #subscribe_to_close(node) {
+    const closeButton = node.querySelector("#close");
+    if (closeButton) {
+      closeButton.addEventListener("click", () => {
+        this.close(node);
+      });
+    }
+  }
+
+  static close(node) {
+    node.setAttribute(this.#key, "true");
+  }
+}
+
 const modal = document.getElementById("modal");
 const modal_secondary = document.getElementById("modal-secondary");
 const modal_three = document.getElementById("modal-three");
-const trigger = document.getElementById("trigger");
 
+const trigger = document.getElementById("trigger");
 const trigger_secondary = document.getElementById("trigger-secondary");
 const trigger_three = document.getElementById("trigger-three");
 
@@ -17,36 +39,3 @@ trigger_secondary.addEventListener("click", () => {
 trigger_three.addEventListener("click", () => {
   Modal.open(modal_three);
 });
-
-class Modal {
-  static key = "aria-hidden";
-
-  static open(node) {
-    if (typeof node !== "object") {
-      throw TypeError();
-    }
-    node.setAttribute(this.key, "false");
-
-    this.#subscribe_to_close(node);
-  }
-
-  static #subscribe_to_close(node) {
-    if (typeof node !== "object") {
-      throw TypeError();
-    }
-
-    const closeButton = node.querySelector("#close");
-    if (closeButton) {
-      closeButton.addEventListener("click", () => {
-        this.close(node);
-      });
-    }
-  }
-
-  static close(node) {
-    if (typeof node !== "object") {
-      throw TypeError();
-    }
-    node.setAttribute(this.key, "true");
-  }
-}
